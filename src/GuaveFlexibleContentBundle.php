71c0cff5a4fbf09904176f49568f7849cde96b35
Christopher Stadler <christopher.stadler@guavestudios.com>
2023-03-20 10:20:41 +0100

    feature: cleanup for contao 4.13 and 5

--- src/GuaveFlexibleContentBundle.php
+++ src/GuaveFlexibleContentBundle.php
@@ -2,19 +2,8 @@
 
 namespace Guave\FlexibleContentBundle;
 
-use Guave\FlexibleContentBundle\DependencyInjection\GuaveFlexibleContentExtension;
-use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class GuaveFlexibleContentBundle extends Bundle
 {
-    /**
-     * Register extension
-     *
-     * @return Extension
-     */
-    public function getContainerExtension(): Extension
-    {
-        return new GuaveFlexibleContentExtension();
-    }
 }
