bea4792e12f9025b8d4c868d5b1474093f2c8857
Christopher Stadler <christopher.stadler@guavestudios.com>
2023-08-04 12:39:30 +0200

    fix: change how templates are rendered

--- src/Controller/ContentElement/FlexibleContentController.php
+++ src/Controller/ContentElement/FlexibleContentController.php
@@ -6,7 +6,6 @@ use Contao\BackendTemplate;
 use Contao\ContentModel;
 use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsContentElement;
-use Contao\CoreBundle\ServiceAnnotation\ContentElement;
 use Contao\FilesModel;
 use Contao\System;
 use Contao\Template;
@@ -16,28 +15,47 @@ use Symfony\Component\HttpFoundation\Response;
 /**
  * @ContentElement("flexibleContent", category="flexibleContent", template="content_element/1col-img")
  */
-#[AsContentElement('flexibleContent', category:'flexibleContent', template:'content_element/1col-img')]
+#[AsContentElement('flexibleContent', category: 'flexibleContent', template: 'content_element/1col-img')]
 class FlexibleContentController extends AbstractContentElementController
 {
+    public function __invoke(Request $request, ContentModel $model, string $section, array $classes = null): Response
+    {
+        $type = $this->getType();
+        if ($model->customTpl === '') {
+            $model->customTpl = 'content_element/' . $model->flexibleTemplate;
+        }
+        $template = $this->createTemplate($model, 'ce_' . $type);
+
+        $this->addHeadlineToTemplate($template, $model->headline);
+        $this->addCssAttributesToTemplate($template, 'ce_' . $type, $model->cssID, $classes);
+        $this->addPropertiesToTemplate($template, $request->attributes->get('templateProperties', []));
+        $this->addSectionToTemplate($template, $section);
+        $this->tagResponse($model);
+
+        $response = $this->getResponse($template, $model, $request);
+
+        if (null === $response) {
+            trigger_deprecation('contao/core-bundle', '4.12', 'Returning null in %s::getResponse() is deprecated, return a Response instead.', static::class);
+            $response = $template->getResponse();
+        }
+
+        return $response;
+    }
+
     protected function getResponse(Template $template, ContentModel $model, Request $request): ?Response
     {
-        if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request)) {
+        $scope = System::getContainer()->get('contao.routing.scope_matcher');
+        if ($scope && $scope->isBackendRequest($request)) {
             $template = new BackendTemplate('be_wildcard');
             $template->title = $model->flexibleTitle;
             $template->wildcard = $model->flexibleSubtitle;
             return $template->getResponse();
         }
 
-        $this->strTemplate = $model->flexibleTemplate;
+        $template->flexibleImages = self::prepareImages($model, 'orderSRC');
+        $template->flexibleImagesColumn = self::prepareImages($model, 'orderSRC2');
 
-        if ($model->customTpl) {
-            $this->strTemplate = $model->customTpl;
-        }
-
-        $model->flexibleImages = self::prepareImages($model, 'orderSRC');
-        $model->flexibleImagesColumn = self::prepareImages($model, 'orderSRC2');
-
-        return $this->render('@Contao/content_element/' . $this->strTemplate . '.html.twig', $model->row());
+        return $template->getResponse();
     }
 
     public static function prepareImages(ContentModel $model, string $attribute): array
@@ -51,7 +69,9 @@ class FlexibleContentController extends AbstractContentElementController
 
         foreach ($images as $image) {
             $file = FilesModel::findByUuid($image);
-            $preparedImages[] = static::getImageData($file);
+            if ($file !== null) {
+                $preparedImages[] = static::getImageData($file);
+            }
         }
 
         $model->$attribute = $preparedImages;
