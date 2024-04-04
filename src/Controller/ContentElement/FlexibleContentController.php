33cf5e9f2be99b359d79343f0e20b35f8e92fc5a
Christopher Stadler <christopher.stadler@guavestudios.com>
2024-04-05 00:42:47 +0200

    feature: get images from files model

--- src/Controller/ContentElement/FlexibleContentController.php
+++ src/Controller/ContentElement/FlexibleContentController.php
@@ -9,6 +9,7 @@ use Contao\ContentModel;
 use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsContentElement;
 use Contao\FilesModel;
+use Contao\StringUtil;
 use Contao\System;
 use Contao\Template;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,8 +21,17 @@ use Symfony\Component\HttpFoundation\Response;
 #[AsContentElement('flexibleContent', category: 'flexibleContent', template: 'content_element/1col-img')]
 class FlexibleContentController extends AbstractContentElementController
 {
+    /**
+     * @param array<string>|null $classes
+     */
     public function __invoke(Request $request, ContentModel $model, string $section, array $classes = null): Response
     {
+        if (isset($GLOBALS['TL_HOOKS']['flexibleTemplateField']) && \is_array($GLOBALS['TL_HOOKS']['flexibleTemplateField'])) {
+            foreach ($GLOBALS['TL_HOOKS']['flexibleTemplateField'] as $callback) {
+                $model = System::importStatic($callback[0])->{$callback[1]}($model, $this);
+            }
+        }
+
         $type = $this->getType();
 
         if ($model->customTpl === '') {
@@ -45,40 +55,45 @@ class FlexibleContentController extends AbstractContentElementController
         return $response;
     }
 
+    /**
+     * @return array<string>|array<array<string>>
+     */
     public static function prepareImages(ContentModel $model, string $attribute): array
     {
-        $preparedImages = [];
-        $images = unserialize($model->$attribute);
-
-        if (empty($images)) {
-            return $preparedImages;
-        }
-
-        foreach ($images as $image) {
-            $file = FilesModel::findByUuid($image);
-
-            if ($file !== null) {
-                $preparedImages[] = static::getImageData($file);
-            }
-        }
-
-        $model->$attribute = $preparedImages;
-
-        return $preparedImages;
-    }
-
-    public static function getImageData(FilesModel $model): array
-    {
-        if (!is_file(TL_ROOT.'/'.$model->path)) {
+        if ($model->$attribute === null) {
             return [];
         }
 
-        $meta = unserialize($model->meta);
+        if (!$model->$attribute || !\is_array(StringUtil::deserialize($model->$attribute))) {
+            return self::getImageData(FilesModel::findByUuid($model->$attribute));
+        }
 
-        if ($meta) {
+        $images = [];
+        $files = FilesModel::findMultipleByUuids(StringUtil::deserialize($model->$attribute));
+
+        foreach ($files as $file) {
+            $images[] = self::getImageData($file);
+        }
+
+        return $images;
+    }
+
+    /**
+     * @return array<string>
+     */
+    public static function getImageData(FilesModel $model): array
+    {
+        $rootDir = System::getContainer()->getParameter('kernel.project_dir');
+
+        if (!is_file($rootDir.'/'.$model->path)) {
+            return [];
+        }
+
+        if ($model->meta) {
+            $meta = unserialize($model->meta);
             $meta = $meta[$GLOBALS['TL_LANGUAGE']];
         } else {
-            $meta['title'] = $model->name;
+            $meta['title'] = $model->title;
         }
 
         return [
@@ -95,7 +110,8 @@ class FlexibleContentController extends AbstractContentElementController
         if ($scope && $scope->isBackendRequest($request)) {
             $template = new BackendTemplate('be_wildcard');
             $template->title = $model->flexibleTitle;
-            $template->wildcard = $model->flexibleSubtitle;
+            $beIcon = $GLOBALS['TL_FLEXIBLE_CONTENT']['iconPath'].'/'.$model->flexibleTemplate.$GLOBALS['TL_FLEXIBLE_CONTENT']['iconExt'];
+            $template->wildcard = '<img src="'.$beIcon.'"><p>'.$model->flexibleTemplate.'</p>';
 
             return $template->getResponse();
         }
